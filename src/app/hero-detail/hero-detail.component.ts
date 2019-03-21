fe7a969c572f7636e58005e366bfb9a039515b4c
Aligator_sah <saixiongh@outlook.com>
2019-03-20 23:20:54 -0400

    Added hero detail and property binding

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Hero} from '../hero'
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroDetailComponent implements OnInit {
 
+  @Input() hero: Hero;
+  
   constructor() { }
 
   ngOnInit() {
